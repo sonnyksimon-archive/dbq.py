bcd445913924d3695053126377d1a3e7771f260b
Sonny Kothapally <me@sonnyksimon.com>
2019-10-19 21:30:00 -0400

    uses return instead of print, so that a webserver can run database queries

--- dbq.py
+++ dbq.py
@@ -13,7 +13,7 @@ def run(connection_string, sql_query):
     conn = db.connect()
     rows = conn.execute(sql_query).fetchall()
     data = [storage(zip(row.keys(), row)) for row in rows]
-    print(publishjson(data))
+    return publishjson(data)
 
 def main(): 
     if len(sys.argv) != 3: 
@@ -21,7 +21,7 @@ def main():
         sys.exit(1)
     else:
         try:
-            run(sys.argv[1], sys.argv[2])
+            print(run(sys.argv[1], sys.argv[2]))
             sys.exit(0)
         except Exception as e:
             print(str(e))
